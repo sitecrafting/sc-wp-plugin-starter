3d9effbc4c747044ce98a6868c339c4e6a06a266
Coby Tamayo <ctamayo@sitecrafting.com>
2020-10-14 12:38:29 -0700

    update setup script to the real deal

--- bin/project-setup.sh
+++ bin/project-setup.sh
@@ -10,24 +10,23 @@ read -p 'snake_case_project_name (e.g. "jedi_academy"): ' SNAKE_NAME
 # Need to do description first, to avoid prematurely replacing "SC PLUGIN" instances
 # that are actually part of "SC PLUGIN DESCRIPTION"
 grep -rl --exclude-dir=.git "SC PLUGIN DESCRIPTION" . | while read f; do echo sed -i'' -e "s/SC PLUGIN DESCRIPTION/${DESCRIPTION}/" $f; done
-grep -rl --exclude-dir=.git "SC PLUGIN" . | while read f; do echo sed -i'' -e "s/SC PLUGIN/${HUMAN_NAME}/" $f; done
-grep -rl --exclude-dir=.git "sc-plugin" . | while read f; do echo sed -i'' -e "s/sc-plugin/${KEBAB_NAME}/" $f; done
-grep -rl --exclude-dir=.git "sc_plugin" . | while read f; do echo sed -i'' -e "s/sc_plugin/${SNAKE_NAME}/" $f; done
-grep -rl --exclude-dir=.git "ScPlugin" .  | while read f; do echo sed -i'' -e "s/ScPlugin/${NAMESPACE}/"   $f; done
-grep -rl --exclude-dir=.git "SC_PLUGIN" . | while read f; do echo sed -i'' -e "s/SC_PLUGIN/${CONST_NAME}/" $f; done
+grep -rl --exclude-dir=.git "SC PLUGIN" . | while read f; do sed -i'' -e "s/SC PLUGIN/${HUMAN_NAME}/" $f; done
+grep -rl --exclude-dir=.git "sc-plugin" . | while read f; do sed -i'' -e "s/sc-plugin/${KEBAB_NAME}/" $f; done
+grep -rl --exclude-dir=.git "sc_plugin" . | while read f; do sed -i'' -e "s/sc_plugin/${SNAKE_NAME}/" $f; done
+grep -rl --exclude-dir=.git "ScPlugin" .  | while read f; do sed -i'' -e "s/ScPlugin/${NAMESPACE}/"   $f; done
+grep -rl --exclude-dir=.git "SC_PLUGIN" . | while read f; do sed -i'' -e "s/SC_PLUGIN/${CONST_NAME}/" $f; done
 
 # There are some instances of certain names that occur twice in one line, so take care of those here.
 # TODO is there an option to sed that does this?
-grep -rl --exclude-dir=.git "sc-plugin" . | while read f; do echo sed -i'' -e "s/sc-plugin/${KEBAB_NAME}/" $f; done
-grep -rl --exclude-dir=.git "ScPlugin" .  | while read f; do echo sed -i'' -e "s/ScPlugin/${NAMESPACE}/"   $f; done
-grep -rl --exclude-dir=.git "SC_PLUGIN" . | while read f; do echo sed -i'' -e "s/SC_PLUGIN/${CONST_NAME}/" $f; done
+grep -rl --exclude-dir=.git "sc-plugin" . | while read f; do sed -i'' -e "s/sc-plugin/${KEBAB_NAME}/" $f; done
+grep -rl --exclude-dir=.git "ScPlugin" .  | while read f; do sed -i'' -e "s/ScPlugin/${NAMESPACE}/"   $f; done
+grep -rl --exclude-dir=.git "SC_PLUGIN" . | while read f; do sed -i'' -e "s/SC_PLUGIN/${CONST_NAME}/" $f; done
 
 echo 'Initializing files...'
 mv sc-plugin.php ${KEBAB_NAME}.php
 mv assets/js/sc-plugin.js assets/js/${KEBAB_NAME}.js
 mv assets/js/admin/sc-plugin-admin.js assets/js/admin/${KEBAB_NAME}-admin.js
 
-exit #TODO
 # Delete this script and start a new Git repo
 rm -rf $0
 rm -rf .git
